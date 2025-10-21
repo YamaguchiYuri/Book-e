51eb9a77217173f5668b5b65435fbcb321c42f83
Yamaguchi Yuri <yurimkmk@gmail.com>
2025-10-21 20:20:49 -0300

    Algumas correçoes e ajuste do formulario

--- login.js
+++ login.js
@@ -1,3 +1,42 @@
+// janela modal
+const dialogWindow = document.querySelector('.pixel_dialog');
+const dialogHeader = document.querySelector('.dialog_header');
+
+let offsetX, offsetY;
+
+const startDrag = (e) => {
+
+    e.preventDefault();
+
+    offsetX = e.clientX - dialogWindow.offsetLeft;
+    offsetY = e.clientY - dialogWindow.offsetTop;
+
+
+    document.addEventListener('mousemove', doDrag);
+    document.addEventListener('mouseup', stopDrag);
+}
+
+
+const doDrag = (e) => {
+
+    let newLeft = e.clientX - offsetX;
+    let newTop = e.clientY - offsetY;
+
+
+    dialogWindow.style.left = newLeft + 'px';
+    dialogWindow.style.top = newTop + 'px';
+}
+
+
+const stopDrag = () => {
+
+    document.removeEventListener('mousemove', doDrag);
+    document.removeEventListener('mouseup', stopDrag);
+}
+
+
+dialogHeader.addEventListener('mousedown', startDrag);
+//fim da janelinha
 document.getElementById('btnCadastrar').addEventListener('click', function() {
     window.location.href = '/cadastro.html';
 });
