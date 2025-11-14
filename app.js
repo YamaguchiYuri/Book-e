2b4a45af3116af3d445511ae5dc9d0a45153a792
Kyu <yuriyamayt@gmail.com>
2025-11-14 10:40:27 -0300

    tentativa de colocar um widget

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 import { initAllWindows, initIcons } from './janelas.js';
 import { initAnotacoes } from './anotacoes.js';
 //import { initNotas } from './notas.js';
-import { initAgenda } from './agenda.js'; // <-- ADICIONE ESTE IMPORT
+import { initAgenda } from './agenda.js';
+import { initAgendaWidget } from './widgetagenda.js';
 
-// --- 1. LÓGICA PRINCIPAL DE AUTENTICAÇÃO ---
+
 const currentUserId = localStorage.getItem('currentUser');
 
-// --- 2. TRAVA DE SEGURANÇA ---
+
 if (!currentUserId) {
-    // ... (código de redirecionamento)
+
 } else {
-    // --- 3. INICIALIZAÇÃO DOS MÓDULOS ---
+
     
     initAllWindows();  
     initIcons();       
     
     initAnotacoes(currentUserId);   
-    //initNotas(currentUserId);
-    initAgenda(currentUserId); // <-- ADICIONE ESTA CHAMADA
-    
+
+    initAgenda(currentUserId);
+    initAgendaWidget(currentUserId);
     console.log(`Aplicativo principal carregado para: ${currentUserId}`);
 }
